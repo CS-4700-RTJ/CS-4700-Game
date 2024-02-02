473113505d694406f860140d0f443feb2227ed12
Joe <joseph.fabel@gmail.com>
2024-02-01 20:05:01 -0800

    Added action handling for Cast Spell

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Input
+    PlayerInput playerInput;
+    private InputAction castSpellAction;
+
+    private void Awake()
     {
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.actions.Enable(); // Make sure player can move at the start
         
+        castSpellAction = playerInput.actions["Cast Spell"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        castSpellAction.started -= CastSpellStarted;
+        castSpellAction.canceled -= CastSpellCanceled;
+    }
+
+    private void Start()
+    {
+        castSpellAction.started += CastSpellStarted;
+        castSpellAction.canceled += CastSpellCanceled;
+    }
+
+    private void CastSpellCanceled(InputAction.CallbackContext context)
+    {
+        print("Mouse released");
+    }
+
+    private void CastSpellStarted(InputAction.CallbackContext context)
+    {
+        print("Mouse Pressed");
     }
 }
