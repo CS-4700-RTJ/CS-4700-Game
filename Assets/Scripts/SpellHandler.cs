48297b10633181b527685ae0d8bdb6f1fa9250a7
Joe <joseph.fabel@gmail.com>
2024-02-02 19:17:03 -0800

    Added UI for spell, health, and mana

--- Assets/Scripts/SpellHandler.cs
+++ Assets/Scripts/SpellHandler.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class SpellHandler : MonoBehaviour
 {
+    [Header("Spell UI")]
+    public Image currentSpellImage;
+
+    public Spell[] availableSpells;
+    
     // Input                                                                         
-    PlayerInput playerInput;
+    private PlayerInput playerInput;
     private InputAction castSpellAction;
     private InputAction cycleSpellAction;
 
+    private int currentSpellIndex;
+    
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -30,14 +39,33 @@ public class SpellHandler : MonoBehaviour
         castSpellAction.started += CastSpellStarted;
         castSpellAction.canceled += CastSpellCanceled;
         cycleSpellAction.performed += OnCycleSpell;
+
+        currentSpellIndex = 0;
     }
 
+    private void SetSelectedSpell(int spellIndex)
+    {
+        currentSpellIndex = spellIndex;
+
+        currentSpellImage.sprite = availableSpells[currentSpellIndex].spellIcon;
+    }
+    
     private void OnCycleSpell(InputAction.CallbackContext context)
     {
         bool scrollForward = context.ReadValueAsButton();
+
+        int newSpellIndex;
+        if (scrollForward)
+        {
+            newSpellIndex = (currentSpellIndex + 1) % availableSpells.Length;
+        }
+        else
+        {
+            newSpellIndex = currentSpellIndex - 1;
+            if (newSpellIndex < 0) newSpellIndex = availableSpells.Length - 1;
+        }
         
-        if (scrollForward) print("Next Spell");
-        else print("Previous Spell");
+        SetSelectedSpell(newSpellIndex);
     }
 
     private void CastSpellCanceled(InputAction.CallbackContext context)
