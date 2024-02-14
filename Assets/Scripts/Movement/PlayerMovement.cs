6d4ca3de20e8fb3e00e6fb5e6c6cd029d449793f
Joe <joseph.fabel@gmail.com>
2024-02-13 23:40:34 -0800

    Did some changes to player movement in PlayerController.cs

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -7,21 +7,13 @@ public class PlayerMovement : MonoBehaviour
 
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool isGrounded;
     public float speed = 5f;
-    public float gravity = -9.8f;
     public float jumpHeight = 3f;
+    
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        isGrounded = controller.isGrounded;
-        
     }
 
     // receive the inputs for our InputManager.cs and apply them to our character controller
@@ -30,22 +22,23 @@ public class PlayerMovement : MonoBehaviour
         Vector3 moveDirection = Vector3.zero;
         moveDirection.x = input.x;
         moveDirection.z = input.y;
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        controller.Move(transform.TransformDirection(moveDirection) * (speed * Time.deltaTime));
 
-        playerVelocity.y += gravity * Time.deltaTime;
-            if (isGrounded && playerVelocity.y < 0)
+        playerVelocity += Physics.gravity * Time.deltaTime;
+            if (controller.isGrounded && playerVelocity.y < 0)
             {
                 playerVelocity.y = -2f;
             }
         controller.Move(playerVelocity *Time.deltaTime);
-    
     }
 
     public void Jump()
     {
-        if (isGrounded)
+        if (controller.isGrounded)
         {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * Physics.gravity.y);
         }
     }
+    
+    
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")] 
+    public float moveSpeed = 5f;
+    public float jumpStrength = 5f;
+    
+    // Input variables
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+
+    private Vector3 moveDirection;
+    private Vector2 lookVector;
+
+    // Movement variables
+    private CharacterController controller;
+    [SerializeField]
+    private float playerJumpVelocity;
+    
+    // Look variables
+    private Camera cam;
+    private float xRotation = 0f;
+    [SerializeField]
+    private float xSensitivity = 10f;
+    [SerializeField]
+    private float ySensitivity = 10f;
+    
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        
+        moveAction = playerInput.actions["Movement"];
+        lookAction = playerInput.actions["Look"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+    private void OnDisable()
+    {
+        moveAction.performed -= OnMove;
+        moveAction.canceled -= OnMove;
+        lookAction.performed -= OnLook;
+        lookAction.canceled -= OnLook;
+        jumpAction.performed -= Jump;
+    }
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main;
+        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        moveAction.performed += OnMove;
+        moveAction.canceled += OnMove;
+        lookAction.performed += OnLook;
+        lookAction.canceled += OnLook;
+        jumpAction.performed += Jump;
+    }
+
+    private void FixedUpdate()
+    {
+        if (controller.isGrounded && playerJumpVelocity < 0)
+        {
+            playerJumpVelocity = -2f;
+        }
+        else
+        {
+            playerJumpVelocity += Physics.gravity.y * Time.fixedDeltaTime;
+        }
+        
+        // Process movement and looking
+        ProcessMove();
+        ProcessLook();
+    }
+
+    // Moves the player according to gravity and player input
+    private void ProcessMove()
+    {
+        controller.Move(transform.TransformDirection(moveDirection) * (moveSpeed * Time.fixedDeltaTime));
+        
+        // Make sure you normalize the move vector so that you don't move faster in a diagonal direction (Pythagorean theorem)
+        Vector3 move = transform.TransformDirection(moveDirection).normalized * (moveSpeed * Time.fixedDeltaTime);
+        move += Vector3.up * (playerJumpVelocity * Time.fixedDeltaTime);
+        
+        controller.Move(move);
+    }
+    
+    // Rotates the camera according to the mouse movement
+    private void ProcessLook()
+    {
+        // calculate camera rotation for looking up and down
+        xRotation -= (lookVector.y * Time.fixedDeltaTime) * ySensitivity;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        // apply this to our camera transform
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        // rotate player to look left and right
+        transform.Rotate(Vector3.up * (lookVector.x * Time.fixedDeltaTime * xSensitivity));
+    }
+    
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        moveDirection = new Vector3(input.x, 0, input.y);
+    }
+
+    private void OnLook(InputAction.CallbackContext context)
+    {
+        lookVector = context.ReadValue<Vector2>();
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (controller.isGrounded)
+        {
+            playerJumpVelocity = jumpStrength;
+        }
+    }
+}
