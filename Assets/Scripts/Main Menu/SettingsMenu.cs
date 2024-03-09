ca48ce37a2c0faf3da80d5aaf7b72967a09856c4
tuannguyen098 <baotuanng@gmail.com>
2024-03-08 18:52:09 -0800

    Music Tracks for temp audio Mixer
     On branch UI

--- Assets/Scripts/Main Menu/SettingsMenu.cs
+++ Assets/Scripts/Main Menu/SettingsMenu.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+   public AudioMixer mixer;
    public void SetVolume(float volume)
    {
-        Debug.Log(volume);
+      mixer.SetFloat("MusicVol", Mathf.Log10(volume) * 20);
    }
 }
